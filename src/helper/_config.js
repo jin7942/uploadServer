7408d7a0679ef9ed0d30568e50ecb93dbcda48d5
jin7942 <infomer.jin@gmail.com>
2022-11-20 04:28:50 +0900

    af

--- src/helper/_config.js
+++ src/helper/_config.js
@@ -4,6 +4,7 @@ import { join, resolve } from 'path';
 const __dirname = resolve();
 
 export default {
+    DOMAIN: 'http://arin.jin7942.co.kr:4000',
     PORT: 4000,
     FILE_MAX_COUNT: 6,
     FILE_MAX_SIZE: 1024 * 1024 * 1024,
--- src/server.js
+++ src/server.js
@@ -9,14 +9,13 @@ import http from 'http';
 import express from 'express';
 import multer from 'multer';
 import { v4 } from 'uuid';
+import sharp from 'sharp';
 
 // 직접 구현한 모듈
 import { mkdirs } from './helper/fileHelper';
 
 // 설정 파일
 import config from './helper/_config.js';
-
-const sharp = require('sharp');
 
 // ### express 기본 설정
 const app = express();
@@ -53,9 +52,7 @@ const storage = multer.diskStorage({
         cb(null, v4() + path.extname(file.originalname));
     },
 });
-
 const upload = multer({ storage: storage });
-
 const filefields = upload.fields([
     {
         name: 'uploadedImage',
@@ -65,19 +62,24 @@ const filefields = upload.fields([
 ]);
 
 // ### API 정의
+
+// 업로드
 app.post('/api/uploadImg', filefields, (req, res) => {
     const resArr = {
         data: [],
     };
+    // 클라이언트에서 받은 이미지
+    // input name과 일치해야함
     const { uploadedImage } = req.files;
 
     let i = 0;
     uploadedImage.map((data) => {
+        // 썸네일로 보일 첫 번째 사지만 리사이징 한다.
         if (i == 0) {
             try {
                 sharp(data.path) // 압축할 이미지 경로
-                    .resize(410, 230) // 리사이징
-                    .withMetadata()
+                    .resize(410, 230) // 리사이징 witdh * height
+                    .withMetadata() // 이미지 정보를 저장해준다
                     .toBuffer((err, buffer) => {
                         if (err) throw err;
                         // 압축된 파일 새로 저장(덮어씌우기)
@@ -90,20 +92,37 @@ app.post('/api/uploadImg', filefields, (req, res) => {
             }
         }
 
+        // 클라이언트에 응답 보낼 데이터 만들기
         resArr.data.push({
-            path: 'http://localhost:4000' + uploadFolderName + '/',
-            originalname: data.originalname,
-            uuidName: data.filename,
-            ext: path.extname(data.originalname),
-            fileSize: data.size,
-            defaultNy: i == 0 ? 1 : 0,
+            path: config.DOMAIN + uploadFolderName + '/', // 업로드 경로(이미지 제공 경로)
+            originalname: data.originalname, // 원본 이름
+            uuidName: data.filename, // uuid 이름
+            ext: path.extname(data.originalname), // 확장자
+            fileSize: data.size, // 파일 사이즈
+            defaultNy: i == 0 ? 1 : 0, // 썸네일 여부
         });
         i++;
     });
     res.json(resArr);
     console.log(resArr);
+
+    // 삭제
+}).delete('/api/uploadImg', async (req, res) => {
+    if (fs.existsSync('./upload/' + req.headers.filename)) {
+        try {
+            fs.unlinkSync('./upload/' + req.headers.filename);
+            console.log('DELETE : ' + req.headers.filename);
+            res.status(200, 'OK');
+            res.send('success');
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        console.log('파일이 존재하지 않습니다.');
+        res.send('fail');
+    }
 });
 
 // ### 서버 시작
 const httpServer = http.createServer(app);
-httpServer.listen(config.PORT, () => console.log('Listening on http://localhost:' + config.PORT));
+httpServer.listen(config.PORT, () => console.log(config.DOMAIN + config.PORT));
