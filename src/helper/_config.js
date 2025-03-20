7f9b4c1b06547b1294c1101cc4a2fa63bfebf4bd
김진범 <infomer.jin@gmail.com>
2025-03-20 18:46:09 +0900

    1

--- src/helper/_config.js
+++ src/helper/_config.js
@@ -1,10 +1,10 @@
-import { join, resolve } from 'path';
+import { resolve } from 'path';
 
 // 프로젝트 폴더 위치 조회
 const __dirname = resolve();
 
 export default {
-    DOMAIN: 'http://arin.jin7942.co.kr',
+    DOMAIN: 'http://localhost',
     PORT: 4000,
     FILE_MAX_COUNT: 6,
     FILE_MAX_SIZE: 1024 * 1024 * 1024,
